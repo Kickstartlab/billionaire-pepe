c858c24a7abb518a43eaa91cf3d1e395d4ff4329
Kickstartlab <info@kickstartlabs.io>
2023-05-05 22:29:54 +0500

    add sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,9 @@ import team_3 from '../assets/team_3.png';
 import top from '../assets/top.png';
 import bg_top from '../assets/bg_top.png';
 import bottom from '../assets/bottom.png';
+import globe from '../assets/globe.png';
+import globe_2 from '../assets/globe_2.png';
+import star from '../assets/star.png';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
@@ -40,6 +43,8 @@ export default function Home() {
 
             <div className="lg:px-20 px-5">
                 <img src={bg_top} alt="Logo" className='absolute top-0 right-0 left-0' />
+
+                <img src={globe_2} alt="Logo" className='absolute right-0 lg:block hidden mt-32' />
 
                 <div className='container mx-auto  relative z-20'>
 
@@ -73,80 +78,67 @@ export default function Home() {
                         </div>
                     </div>
 
-                </div>
-            </div>
+                    <div id='about' className='mt-12'>
+                        <p className='font-inter text-center font-semibold text-white-100 pb-5'>
+                            THE COLLECTION
+                        </p>
 
-            {/* about us section */}
+                        <h3 className='md:text-5xl text-2xl font-cehua text-center font-bold text-white-100'>
+                            Fast, Simple & Secure
+                        </h3>
 
-            {/* <div id='about' className="lg:px-20 px-5">
-                <div className='container mx-auto'>
+                        <img src={globe} alt="Logo" className='absolute right-0 w-1/4 -mt-12 lg:block hidden' />
 
-                    <div className="flex flex-col justify-center text-center items-center gap-x-12 py-12">
+                        <div className="lg:flex justify-center items-center gap-x-12 py-12 space-y-12">
 
-                        <div data-aos="fade-left" className="lg:w-1/2 w-full md:mt-0 mt-12">
-                            <h3 className='font-poppins font-semibold text-white-50 mb-6'>
-                                About us
-                            </h3>
+                            <div className='flex flex-col lg:gap-y-28 gap-y-12'>
+                                <div className='lg:text-right'>
+                                    <h3 className='text-2xl font-cehua font-bold text-white-100'>
+                                        Lorem
+                                    </h3>
+                                    <p className='leading-loose'>
+                                        Find digital artwork by professionals and discover the true meaning of the artwork
+                                    </p>
+                                </div>
 
-                            <h2 className='md:text-4xl text-3xl font-cehua font-semibold text-white-100'>
-                                Fast, Simple & Secure
-                            </h2>
-
-                            <p className='pt-6 pb-4 text-white-50 font-poppins'>
-                                It is a long established fact that a reader will be distracted by the readable content of a page
-                            </p>
-
-                        </div>
-
-                        <div className='grid lg:grid-cols-3 grid-cols-1 gap-8 items-center flex-wrap mt-8'>
-
-                            <div className='flex flex-col gap-y-5 items-center justify-center'>
-                                <img src={sec_1} alt="Logo" className='' />
-
-                                <p className='text-white-50 font-poppins'>
-                                    It is a long established fact
-                                    that a reader will be distracted by the readable
-                                    content of a page
-                                </p>
-
-                                <h2 className='md:text-4xl text-3xl font-poppins font-bold text-purple-50'>
-                                    MEME
-                                </h2>
+                                <div className='lg:text-right'>
+                                    <h3 className='text-2xl font-cehua font-bold text-white-100'>
+                                        Lorem
+                                    </h3>
+                                    <p className='leading-loose'>
+                                        Find digital artwork by professionals and discover the true meaning of the artwork
+                                    </p>
+                                </div>
                             </div>
 
-                            <div className='flex flex-col gap-y-5 items-center justify-center'>
-                                <img src={sec_2} alt="Logo" className='' />
-
-                                <p className='text-white-50 font-poppins'>
-                                    It is a long established fact
-                                    that a reader will be distracted by the readable
-                                    content of a page
-                                </p>
-
-                                <h2 className='md:text-4xl text-3xl font-poppins font-bold text-purple-50'>
-                                    UTILITY
-                                </h2>
+                            <div>
+                                <img src={top} alt="Logo" className='flex mx-auto' />
                             </div>
 
-                            <div className='flex flex-col gap-y-5 items-center justify-center'>
-                                <img src={sec_3} alt="Logo" className='' />
+                            <div className='flex flex-col gap-y-28'>
+                                <div className='lg:block hidden'>
 
-                                <p className='text-white-50 font-poppins'>
-                                    It is a long established fact
-                                    that a reader will be distracted by the readable
-                                    content of a page
-                                </p>
+                                </div>
 
-                                <h2 className='md:text-4xl text-3xl font-poppins font-bold text-purple-50'>
-                                    HUGE MARKET
-                                </h2>
+                                <div className=''>
+                                    <h3 className='text-2xl font-cehua font-bold text-white-100'>
+                                        Lorem
+                                    </h3>
+                                    <p className='leading-loose'>
+                                        Find digital artwork by professionals and discover the true meaning of the artwork
+                                    </p>
+                                </div>
                             </div>
+
+                        <img src={star} alt="Logo" className='absolute right-44 mt-12 lg:block hidden' />
+
+                        <img src={star} alt="Logo" className='absolute left-1/3 -ml-16 translate-y-52 lg:block hidden' />
 
                         </div>
                     </div>
 
                 </div>
-            </div> */}
+            </div>
 
             {/* tokenomics section */}
 
@@ -479,9 +471,9 @@ export default function Home() {
                             </h2>
 
                             <div className='flex items-center gap-x-8 mt-6 space-y-4 flex-wrap'>
-                                <img src={list_1} alt='token' className='flex mx-auto lg:w-full w-1/4'></img>
-                                <img src={list_2} alt='token' className='flex mx-auto lg:w-full w-1/4'></img>
-                                <img src={list_3} alt='token' className='flex mx-auto lg:w-full w-1/4'></img>
+                                <img src={list_1} alt='token' className='flex mx-auto w-1/4'></img>
+                                <img src={list_2} alt='token' className='flex mx-auto w-1/4'></img>
+                                <img src={list_3} alt='token' className='flex mx-auto w-1/4'></img>
                             </div>
                         </div>
 
